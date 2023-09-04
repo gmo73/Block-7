45863f9a6aa5ffd343041a0b669a15663a69535f
gmo73 <monroygmo73@gmail.com>
2023-09-04 08:23:21 -0500

    updateJS

---
+++ app.js
@@ -0,0 +1,19 @@
+// 1st combination Number 
+
+function difference (a, b) { //subtraction
+    return Math.abs(a - b);
+} //expected result 10
+
+console.log(difference(12, 2));
+
+//2nd combination number
+
+    let a=10;
+    let b=20;
+    let c=10;
+    console.log((a+b+c)); //addition
+    // expected reult 40
+
+// 3rd combination number
+
+
